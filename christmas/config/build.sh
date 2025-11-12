3bd61e6904d43a14c577b625613e1159858a6a28
Chris Pfohl <christopher.pfohl@gmail.com>
2025-11-12 12:20:52 -0500

    Get Poppins working!

--- christmas/config/build.sh
+++ christmas/config/build.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-chordpro --debug --config=./config/chordpro.json --filelist ./config/songbook.chordpro --output ./config/songbook.pdf
+chordpro --config=./config/chordpro.json --filelist ./config/songbook.chordpro --output ./config/songbook.pdf
