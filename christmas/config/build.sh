cdfc83c28a09ae0414536e17d87cc7158734390f
Chris Pfohl <christopher.pfohl@gmail.com>
2025-11-17 13:45:59 -0500

    Add The Christmas Song

--- christmas/config/build.sh
+++ christmas/config/build.sh
@@ -2,4 +2,7 @@
 
 cd "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-chordpro --config=./config/config.json --filelist ./config/songbook.chordpro --output ./config/songbook.pdf
+chordpro \
+    --config=./config/config.json \
+    --filelist ./config/songbook.chordpro \
+    --output ./config/songbook.pdf
